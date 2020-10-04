4f27a588bf5a23ce61b0814f8a29e6c00b98a2a4
dong <18903105781@163.com>
2020-10-04 11:40:11 +0800

    Initial commit

---
+++ src/main/java/com/leetcode/ProblemsetApplication.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemsetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProblemsetApplication.class, args);
+    }
+
+}
